9962fbfb4e0166e64e0712f4fef5bbcad40d75c8
Konrad Weiß <konrad@schwimmteamerzgebirge.de>
2023-10-27 23:16:00 +0200

    initialize matrix correctly

--- tidal-harvest/src/app/core/model/Matrix.ts
+++ tidal-harvest/src/app/core/model/Matrix.ts
@@ -4,9 +4,15 @@ import {Farmland} from "./field/Farmland";
 export class Matrix {
 
     private _content: Field[][] = [];
+    private readonly _x: number;
+    private readonly _y: number;
 
     public constructor(x: number, y: number) {
+        this._x = x;
+        this._y = y;
+
         for (let i = 0; i < x; i++) {
+            this._content[i] = [];
             for (let j = 0; j < y; j++) {
                 this._content[i][j] = new Farmland(i, j);
             }
@@ -17,4 +23,12 @@ export class Matrix {
         return this._content;
     }
 
+
+    get x(): number {
+        return this._x;
+    }
+
+    get y(): number {
+        return this._y;
+    }
 }
