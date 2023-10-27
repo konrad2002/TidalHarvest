3398c6cdd667d569997105151c2afa2a7c004322
Paul Franosch <entjic@gmail.com>
2023-10-27 23:47:29 +0200

    fix method contract

--- tidal-harvest/src/app/core/game/Game.ts
+++ tidal-harvest/src/app/core/game/Game.ts
@@ -1,6 +1,9 @@
 import {Matrix} from "../model/Matrix";
 import {TickMachine} from "./TickMachine";
 import {Field} from "../model/field/Field";
+import {FieldType} from "../model/field/FieldType";
+import {Farmland} from "../model/field/Farmland";
+import {Farmer} from "../model/field/Farmer";
 
 export class Game {
 
@@ -12,7 +15,19 @@ export class Game {
         this._tickMachine = new TickMachine(this._matrix);
     }
 
-    public place(field: Field){
+    public place(fieldType: FieldType, x: number, y: number){
+        let field: Field;
+        switch (fieldType) {
+            case FieldType.FARMLAND:
+                field = new Farmland(x, y);
+                break;
+            case FieldType.FARMER:
+                field = new Farmer(x, y);
+                break;
+            case FieldType.ROCK:
+                throw new Error();
+                break;
+        }
         this._tickMachine.changeField(field);
     }
 
